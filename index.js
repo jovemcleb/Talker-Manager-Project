c1b702e8b5669414e6d66ff2b7f0cd92b4fc6616
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-06-22 20:20:56 -0300

    :sparkles: feat: Requisito 1 - OK - Retorna o array de palestrantes

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const { read } = require('./src/helpers/index');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  try {
+    const talker = await read();
+
+    console.log(talker);
+
+    return res.status(200).json(talker);
+  } catch (e) {
+    return res.status(404).json({ message: 'Palestrantes não encontrados' });    
+  }
 });
 
 app.listen(PORT, () => {
---
+++ src/helpers/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs/promises');
+
+const read = async () => {
+  try {
+    const talkers = await fs.readFile('talker.json', 'utf-8');
+    return JSON.parse(talkers);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const write = async (content) => {
+  try {
+    await fs.writeFile('database.json', JSON.stringify(content));
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+module.exports = {
+  read,
+  write,
+};
